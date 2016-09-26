f962060660bd5b2e7d401d08a810e1b304b07e46
Adam Andreasson <edogawa0@gmail.com>
2016-09-26 14:39:05 +0200

    Removed title bar from theme, fixed camera strings

--- KasslR/app/src/main/java/gruppn/kasslr/Feed.java
+++ KasslR/app/src/main/java/gruppn/kasslr/Feed.java
@@ -9,12 +9,14 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 public class Feed extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_feed);
         requestCameraPermission();
     }
