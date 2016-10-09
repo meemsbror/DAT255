460cf8bf95b5b998bdc2c9ed1f77474534be8560
Alle <Algotify@gmail.com>
2016-10-09 16:07:41 +0200

    glömde commita alla filer

---
+++ KasslR/app/src/main/java/gruppn/kasslr/ProfileInformation.java
@@ -0,0 +1,53 @@
+package gruppn.kasslr;
+
+import android.widget.Switch;
+
+/**
+ * Created by admin on 2016-10-01.
+ */
+
+public class ProfileInformation {
+
+    public int score = 0;
+    public String name = "Namn";
+
+    private final int COMPLETE_VOCABULARY_SCORE = 2;
+    private final int COMPLETE_ACHIEVEMENT_SCORE = 20;
+    private final int CREATE_VOCABULARY_SCORE = 25;
+
+    public enum CompletedAction{
+        COMPLETE_VOCABULARY, CREATE_VOCABULARY, ACHIEVEMENT
+    }
+
+    public ProfileInformation(){
+        //Todo, load name and score from DB
+        //score = DB.getScore
+        //name = DB.getName
+    }
+
+    public void incScore(CompletedAction completedAction) {
+        switch (completedAction) {
+            case CREATE_VOCABULARY:
+                score += CREATE_VOCABULARY_SCORE;
+                break;
+            case COMPLETE_VOCABULARY:
+                score += COMPLETE_VOCABULARY_SCORE;
+                break;
+            case ACHIEVEMENT:
+                score += COMPLETE_ACHIEVEMENT_SCORE;
+                break;
+        }
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
