ee4ac395046976039198b658aca74923512844d8
Adam Andreasson <edogawa0@gmail.com>
2016-10-28 16:52:58 +0200

    Added latest revision of web server

--- Kasslr-Web/skalman.php
+++ Kasslr-Web/skalman.php
@@ -20,6 +20,45 @@
 
 	switch ($action) {
 
+
+/**
+
+*/
+
+case 'feed':
+
+$find = $db->prepare('SELECT v.title, u.id as userid, v.id as vocabulary, u.name FROM vocabularies v JOIN users u ON u.id = v.owner ORDER BY v.submitted DESC, v.id DESC');
+$find->execute();
+$result = $find->fetchAll();
+
+$feed = array();
+
+foreach ($result as $row) {
+
+	$finder = $db->prepare('SELECT * FROM items WHERE vocabulary = ?');
+	$finder->execute(array($row["vocabulary"]));
+	$itemArray = $finder->fetchAll();
+
+	$items = array();
+
+	foreach ($itemArray as $item) {
+		$itemUrl = $baseUrl.'user_images/' . $row["userid"] . "-" . $row["vocabulary"] . "-" . $item["id"] . ".jpg";
+		$items[] = array($item["word"], $itemUrl);
+	}
+
+	$owner = array("id" => $row["userid"], "name" => $row["name"], "image" => $baseUrl . 'user_images/' . $row["userid"] . ".jpg");
+
+	$feed[] = array("id"=>$row["vocabulary"], "title"=>$row["title"], "owner"=>$owner, "items"=>$items);
+
+}
+
+$response = array("feed" => $feed);
+
+header('Content-Type: application/json;charset=utf-8');
+echo json_encode($response);
+
+break;
+
 /**
 
 */
@@ -41,8 +80,6 @@ $response = array("user"=>null, "name"=>"Anonym", "score"=>0);
 
 		// user is not registered lul
 if(sizeof($result) < 1){
-
-	var_dump("not registered lul");
 
 	$request = $db->prepare('INSERT INTO users (id) VALUES (?)');
 	$request->execute(array($userId));
@@ -71,6 +108,7 @@ case 'new_vocabulary':
 $userId = preg_replace("/[^a-zA-Z0-9]+/", "", $_POST["user"]);
 $title = $_POST["title"];
 
+
 $request = $db->prepare('INSERT INTO vocabularies (owner, title) VALUES (?, ?)');
 $request->execute(array($userId, $title));
 $id = $db->lastInsertId();
@@ -98,13 +136,53 @@ $request = $db->prepare('INSERT INTO items (vocabulary, word) VALUES (?, ?)');
 $request->execute(array($vocabularyId, $word));
 $id = $db->lastInsertId();
 
-$fileName = "user_images/" . $userId . "-" . $vocabularyId . "-" . $id . ".jpg";
+$width = 720;
+$height = 960;
+
+list($w, $h) = getimagesize($_FILES['image']['tmp_name']);
 
 
-$good = move_uploaded_file($_FILES['image']['tmp_name'], $fileName);
+$ratio = max($width/$w, $height/$h);
+$h = ceil($height / $ratio);
+$x = ($w - $width / $ratio) / 2;
+$w = ceil($width / $ratio);
+
+$path = "user_images/" . $userId . "-" . $vocabularyId . "-" . $id . ".jpg";
+$imgString = file_get_contents($_FILES['image']['tmp_name']);
+
+$image = imagecreatefromstring($imgString);
+$tmp = imagecreatetruecolor($width, $height);
+imagecopyresampled($tmp, $image,
+	0, 0,
+	$x, 0,
+	$width, $height,
+	$w, $h);
+
+imagejpeg($tmp, $path, 80);
+
+imagedestroy($image);
+imagedestroy($tmp);
 
 $response = array("itemId"=>$id, "uploaded"=>$good);
 
+header('Content-Type: application/json;charset=utf-8');
+echo json_encode($response);
+
+break;
+/**
+
+*/
+
+case 'update_user':
+
+$userId = preg_replace("/[^a-zA-Z0-9]+/", "", $_POST["user"]);
+$name = $_POST["name"];
+
+
+$request = $db->prepare('UPDATE users SET name = ? WHERE id = ?');
+$request->execute(array($name, $userId));
+
+$response = array("nehedu"=>true);
 header('Content-Type: application/json;charset=utf-8');
 echo json_encode($response);
 
